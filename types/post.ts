0ed1dd4ddc4f87458f84acd06adb7aa59af875b1
MrVyde <Mrvyde@outlook.com>
2026-05-21 17:24:49 +0100

    update Post type to include comments and required fields

--- types/post.ts
+++ types/post.ts
@@ -1,12 +1,16 @@
+import { Comment } from './comment';
+
 export interface Post {
   id: string;
   title: string;
   content: string;
   createdAt: string;
-  updatedAt?: string;
+  updatedAt: string;  // Required now
 
-  author?: {
+  author: {
     id: string;
     username: string;
   };
+
+  comments: Comment[]; 
 }
